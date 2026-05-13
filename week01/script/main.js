0eb944833040a96ee04c03e5cae975f4fe1b0e43
Agatha Odeh <agathaokofulds.com>
2026-05-13 01:56:32 +0100

    First week assignment

---
+++ week01/script/main.js
@@ -0,0 +1,5 @@
+// Set current year in footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById('lastModified').textContent = document.lastModified;
